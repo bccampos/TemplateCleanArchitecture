d23d96b07628a464f2804eafa1a17548fd4feb91
Bruno Campos <bruno.campos@flipdish.com>
2023-04-21 20:02:05 +0100

    Add and configure JWT token

--- bruno.Application/Authentication/AuthenticationService.cs
+++ bruno.Application/Authentication/AuthenticationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bruno.Application.Common.Interfaces.Authentication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,20 @@ namespace bruno.Application.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
-        public AuthenticationResult Register(string firstName, string LastName, string email, string password)
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
         {
-            return new AuthenticationResult(Guid.NewGuid(), firstName, LastName, email, "token");
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }   
+
+        public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+        {
+            Guid userId = Guid.NewGuid();
+
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+            return new AuthenticationResult(userId, firstName, lastName, email, token);
         }
 
         public AuthenticationResult Login(string email, string password)
--- bruno.Infrastructure/DependencyInjection.cs
+++ bruno.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using bruno.Application.Common.Interfaces.Authentication;
+using bruno.Application.Common.Interfaces.Services;
+using bruno.Infrastructure.Authentication;
+using bruno.Infrastructure.Authentication.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace bruno.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
-        { 
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
             return services;    
         }
 
